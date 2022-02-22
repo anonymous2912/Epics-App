5b31dc64d8d376e1a43840475628bd0110b78146
soumya1810 <66905516+soumya1810@users.noreply.github.com>
2022-02-22 22:45:13 +0530

    Add files via upload

---
+++ Soumya_files/DoctorList.py
@@ -0,0 +1,62 @@
+from kivy.lang import Builder
+
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.app import MDApp
+from kivymd import images_path
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelThreeLine
+
+KV = '''
+<Content>
+    size_hint_y: None
+    height: self.minimum_height
+
+    TwoLineIconListItem:
+        text: "(050)-123-45-67"
+        secondary_text: "Mobile"
+
+        IconLeftWidget:
+            icon: 'phone'
+
+
+MDScreen:
+
+    MDBoxLayout:
+        orientation: "vertical"
+
+        MDToolbar:
+            title: "Contact List Of Doctors"
+            elevation: 10
+
+        ScrollView:
+
+            MDGridLayout:
+                cols: 1
+                adaptive_height: True
+                id: box
+'''
+
+
+class Content(MDBoxLayout):
+    pass
+
+
+class Test(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+    def on_start(self):
+        for i in range(10):
+            self.root.ids.box.add_widget(
+                MDExpansionPanel(
+                    icon=f"{images_path}kivymd_logo.png",
+                    content=Content(),
+                    panel_cls=MDExpansionPanelThreeLine(
+                        text="Text",
+                        secondary_text="Secondary text",
+                        tertiary_text="Tertiary text",
+                    )
+                )
+            )
+
+
+Test().run()
